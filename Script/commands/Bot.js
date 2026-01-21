df7f2a1ed276471af70efac2ba0980d3ff62d453
sahu892 <samiulislam88306506@gmail.com>
2026-01-21 14:56:59 +0600

    Update Bot.js

--- Script/commands/Bot.js
+++ Script/commands/Bot.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "bot",
   version: "1.0.0",
   hasPermission: 0,
-  credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦",
+  credits: "Samiul 𝐈𝐬𝐥𝐚𝐦",
   description: "Random fun reply when someone says Bot",
   commandCategory: "Fun",
   usages: "[Bot]",
