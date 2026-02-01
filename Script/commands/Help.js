b83c1cde2f2a9a6549ffe40d2501e7eaa97a947f
sahu892 <samiulislam88306506@gmail.com>
2026-02-02 00:49:38 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -43,10 +43,10 @@ module.exports.languages = {
 
 // 🔹 এখানে আপনার ফটো Imgur লিংক করে বসাবেন ✅
 const helpImages = [
-    "https://i.imgur.com/sxSn1K3.jpeg",
+    "",
     "https://i.imgur.com/8WvpgUL.jpeg",
-    "https://i.imgur.com/8WvpgUL.jpeg",
-    "https://i.imgur.com/sxSn1K3.jpeg"
+    "",
+    ""
 ];
 
 
